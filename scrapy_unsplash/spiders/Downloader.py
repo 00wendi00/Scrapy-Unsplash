285d2b308abc318a776e977b7769b355c502ce95
wendi <842876912@qq.com>
2019-06-01 02:39:43 +0800

    PEP8

--- scrapy_unsplash/spiders/Downloader.py
+++ scrapy_unsplash/spiders/Downloader.py
@@ -1,39 +1,57 @@
-# -*- encoding:utf-8 -*-
-import sqlite3, urllib.request, threadpool  # sqlite3：管理数据库；urllib：下载文件；threadpool：线程池
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : test.py
+# @Author: Wade Cheung
+# @Date  : 2018/6/13
+# @Desc  :
+
+
+import sqlite3
+import threadpool
+
+from urllib import request
 
 
 class Downloader:
     def __init__(self, urls, folder, threads=10):  # 初始化函数
-        self.urls = urls  # 需要下载的网址list
-        self.folder = folder  # 保存的文件目录
-        self.threads = threads  # 并发线程数
+        """
+        :param urls: 需要下载的网址list
+        :param folder: 保存的文件目录
+        :param threads: 并发线程数
+        """
+        self.urls = urls
+        self.folder = folder
+        self.threads = threads
 
     def run(self):
-        pool = threadpool.ThreadPool(self.threads)  # 新建线程池
-        requests = threadpool.makeRequests(self.downloader, self.urls)  # 新建请求
-        [pool.putRequest(i) for i in requests]  # 将请求装入线程池
-        pool.wait()  # 等待运行完成
+        pool = threadpool.ThreadPool(self.threads)
+        requests = threadpool.makeRequests(self.downloader, self.urls)
+        # 将请求装入线程池, 等待运行完成
+        [pool.putRequest(i) for i in requests]
+        pool.wait()
 
     def downloader(self, url):
         pre = url.split('/')[-1]
-        # name = pre if pre.split(".")[-1] in ["jpg", "png", "bmp"] else pre + ".jpg"  # 文件名
+        # name = pre if pre.split(".")[-1] in ["jpg", "png", "bmp"] else pre + ".jpg"
         name = pre[:19] + ".jpg"  # 文件名
         print(self.folder + "/" + name)
-        self.auto_down(url, self.folder + "/" + name)  # 下载
+        self.auto_down(url, self.folder + "/" + name)
 
     def auto_down(self, url, filename):  # 处理出现网络不好的问题，重新下载
         try:
-            urllib.request.urlretrieve(url, filename)
-        except urllib.request.URLError as e:
+            request.urlretrieve(url, filename)
+        except request.URLError as e:
             print(str(e) + 'Network Error,redoing download :' + url)
-            self.auto_down(url, filename)  # 递归
+            self.auto_down(url, filename)
 
 
 if __name__ == "__main__":
     urls = []
-    conn = sqlite3.connect("e:/wendi1/pyplace/scrapy_unsplash/database/link.db")  # 连接数据库
+    conn = sqlite3.connect(
+        "e:/wendi1/pyplace/scrapy_unsplash/database/link.db")
     cursor = conn.execute("SELECT LINK FROM LINK WHERE ID <= 30")  # 选择总数
     for row in cursor:
         urls.append(row[0])
-    pd = Downloader(urls, "e:/wendi1/pyplace/scrapy_unsplash/files", threads=50)  # 新建下载器
-    pd.run()
+    download = Downloader(urls, "e:/wendi1/pyplace/scrapy_unsplash/files",
+                          threads=50)
+    download.run()
--- scrapy_unsplash/spiders/unsplash.py
+++ scrapy_unsplash/spiders/unsplash.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# @File  : test.py
+# @Author: Wade Cheung
+# @Date  : 2018/6/13
+# @Desc  :
+
+
 import json
 import sqlite3
 import threading
 import scrapy
 from scrapy.spiders import CrawlSpider
+
+URL0 = "https://api.unsplash.com/photos/?client_id=fa60305aa82e74134cabc7093ef54c8e2c370c47e73152f72371c828daedfcd7&page="
+PATH = "E:\\wendi1\\pyplace\\scrapy_unsplash\\database\\link.db"
 
 
 class UnsplashSpider(CrawlSpider):
@@ -11,32 +21,36 @@ class UnsplashSpider(CrawlSpider):
     allowed_domains = ['api.unsplash.com']
 
     def start_requests(self):
-        createDB()  # 创建数据库
+        createDB()  # 创建db
 
-        start, page = 1, 1,  # 要爬的页面数
+        start, page = 1, 100  # 要爬的页面数
         for i in range(start, page + 1):  # 从第一页开始
-            url = 'https://api.unsplash.com/photos/?client_id=fa60305aa82e74134cabc7093ef54c8e2c370c47e73152f72371c828daedfcd7&page=' + str(
-                i) + '&per_page=30'
+            url = URL0 + str(i) + '&per_page=30'
             yield scrapy.Request(url=url, callback=self.parse_item)
 
     def parse_item(self, response):
-        conn = sqlite3.connect("E:\\wendi1\\pyplace\\scrapy_unsplash\\database\\link.db")  # 连接数据库
+        conn = sqlite3.connect(PATH)
         print('-------------------')
 
-        js = json.loads(str(response.body_as_unicode()))  # 读取响应body，并转化成可读取的json
+        js = json.loads(
+            str(response.body_as_unicode()))  # 读取响应body
         for j in js:
             link = j["urls"]["raw"]
-            sql = "INSERT INTO LINK(LINK) VALUES ('%s');" % link  # 将link插入数据库
+            sql = "INSERT INTO LINK(LINK) VALUES ('%s');" % link
             conn.execute(sql)
-        semaphore = threading.Semaphore(1)  # 引入线程信号量，避免写入数据库时死锁
-        semaphore.acquire()  # P操作
-        conn.commit()  # 写入数据库，此时数据库文件独占
-        semaphore.release()  # V操作
+        semaphore = threading.Semaphore(1)  # 线程信号量，避免写入数据库时死锁
+        semaphore.acquire()
+        conn.commit()
+        semaphore.release()
 
 
-def createDB():  # 创建数据库
-    conn = sqlite3.connect("E:\\wendi1\\pyplace\\scrapy_unsplash\\database\\link.db")  # Sqlite是一个轻量数据库，不占端口，够用
+def createDB():
+    """
+    创建db
+    :return:
+    """
+    conn = sqlite3.connect(
+        "E:\\wendi1\\pyplace\\scrapy_unsplash\\database\\link.db")
     conn.execute("DROP TABLE IF EXISTS LINK;")  # 重新运行删掉数据库
-    conn.execute("CREATE TABLE LINK ("  # 创建属性ID：主键自增；属性LINK：存放图片链接
-                 "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
-                 "LINK VARCHAR(255));")
+    conn.execute(
+        "CREATE TABLE LINK (ID INTEGER PRIMARY KEY AUTOINCREMENT,LINK VARCHAR(255));")
